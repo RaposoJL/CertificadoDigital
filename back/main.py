5ba050ac9028eb9790de8db64f518a00850fce0d
RaposoJL <raposojlucas@gmail.com>
2024-05-20 20:58:45 -0300

    Primeiro

---
+++ back/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from routes.home import home_route
+from  routes.alunos import aluno_route
+
+app = Flask(__name__)
+
+app.register_blueprint(home_route)
+app.register_blueprint(aluno_route, url_prefix='/alunos')
+
+app.run(debug=True)
